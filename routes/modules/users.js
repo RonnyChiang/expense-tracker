ab791206a9ca24c1609e22c57f476e09c0a7fef0
ronnychiang <ronnychiang1164@gmail.com>
2021-12-24 18:05:37 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,7 @@
 //introduce
 const express = require('express')
 const router = express.Router()
-// const User = require("../../models/user")
+const User = require("../../models/user")
 // const passport = require("passport")
 // const bcrypt = require("bcryptjs")
 // route
@@ -14,5 +14,35 @@ router.get("/register", (req, res) => {
   res.render("register")
 })
 
+router.post("/register", (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log("User already exists.")
+        res.render("register", {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect("/"))
+
+      }
+    })
+  // .catch(err => {
+  //   res.render(
+  //     'errorPage',
+  //     { status: 500, error: err.message }
+  //   )
+  // })
+
+})
 
 module.exports = router
