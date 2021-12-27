0e7dda12d5a91aebc6c7b33476f2b9381c77124d
Wendell Liu <cuk.bas@gmail.com>
2021-12-27 23:22:52 +0800

    feat: refactor the process

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,53 +1,43 @@
 // require
-const Record = require("../record")
-const User = require("../user")
-const Category = require("../category")
+const Record = require("../record");
+const User = require("../user");
+const Category = require("../category");
 
-const bcrypt = require("bcryptjs")
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+const bcrypt = require("bcryptjs");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const db = require('../../config/mongoose')
-
+const db = require("../../config/mongoose");
 
 db.once("open", () => {
-  const userIds = Array.from({ length: 1 }, (v, i) => i)
-  let categoryItems = {}
+  const userIds = Array.from({ length: 1 }, (v, i) => i);
+  let categoryItems = {};
 
-  return (
-    Promise.all(
-      userIds.map(uId =>
-        bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash("12345678", salt))
-          .then(hash =>
-            User.create({
-              name: `user${uId + 1}`,
-              email: `user${uId + 1}@example.com`,
-              password: hash
-            })
-          )
+  return Category.find({})
+    .then(categories => {
+      return Promise.all(
+        userIds.map(uId =>
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash("12345678", salt))
+            .then(hash =>
+              User.create({
+                name: `user${uId + 1}`,
+                email: `user${uId + 1}@example.com`,
+                password: hash
+              })
+            )
+            .then(user => {
+              const userId = user._id;
+              const recordIds = Array.from({ length: 10 }, (v, i) => i);
 
-          .then(Category.find(function (err, categoryItem) {
-            if (err) {
-              console.error(err);
-            } else {
-              categoryItems = categoryItem
-            }
-          }))
+              return Promise.all(
+                recordIds.map(recordId => {
+                  // fetch category you want here
+                  const category = categories[0];
+                  const categoryId = category._id;
 
-          .then(user => {
-            console.log(categoryItems[1]._id)
-            const userId = user._id
-            const recordIds = Array.from({ length: 10 }, (v, i) => i)
-
-            return Promise.all(
-              categoryItems.map(cId => {
-                const categoryId = cId._id
-                console.log(categoryId)
-                return recordIds.map(rId => {
-                  console.log(rId)
                   return Record.create({
                     name: `Item${rId + 1}`,
                     date: `1991-06-01`,
@@ -55,20 +45,12 @@ db.once("open", () => {
                     description: `item${rId + 1}`,
                     userId: userId,
                     categoryId: categoryId
-                  })
-                }
-
-
-
-                )
-              })
-            )
-
-          })
-      )
-
-    )
-      .finally(() => process.exit())
-
-  )
-})
+                  });
+                })
+              );
+            })
+        )
+      );
+    })
+    .finally(() => process.exit());
+});
