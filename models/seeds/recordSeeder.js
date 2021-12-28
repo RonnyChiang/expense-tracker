3d6e4349b7173dbc216152a4082875908b2f1118
ronnychiang <ronnychiang1164@gmail.com>
2021-12-28 15:50:21 +0800

    fix: recordseeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -12,63 +12,48 @@ const db = require('../../config/mongoose')
 
 
 db.once("open", () => {
-  const userIds = Array.from({ length: 1 }, (v, i) => i)
-  let categoryItems = {}
+  const userIds = Array.from({ length: 1 }, (v, i) => i);
 
-  return (
-    Promise.all(
-      userIds.map(uId =>
-        bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash("12345678", salt))
-          .then(hash =>
-            User.create({
-              name: `user${uId + 1}`,
-              email: `user${uId + 1}@example.com`,
-              password: hash
-            })
-          )
-
-          .then(Category.find(function (err, categoryItem) {
-            if (err) {
-              console.error(err);
-            } else {
-              categoryItems = categoryItem
-            }
-          }))
-
-          .then(user => {
-            console.log(categoryItems[1]._id)
-            const userId = user._id
-            const recordIds = Array.from({ length: 10 }, (v, i) => i)
-
-            return Promise.all(
-              categoryItems.map(cId => {
-                const categoryId = cId._id
-                console.log(categoryId)
-                return recordIds.map(rId => {
-                  console.log(rId)
-                  return Record.create({
-                    name: `Item${rId + 1}`,
-                    date: `1991-06-01`,
-                    amount: `${1000 + rId + 1}`,
-                    description: `item${rId + 1}`,
-                    userId: userId,
-                    categoryId: categoryId
-                  })
-                }
-
-
-
-                )
+  return Category.find({})
+    .then(categories => {
+      return Promise.all(
+        userIds.map(uId =>
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash("12345678", salt))
+            .then(hash =>
+              User.create({
+                name: `user${uId + 1}`,
+                email: `user${uId + 1}@example.com`,
+                password: hash
               })
             )
+            .then(user => {
+              const userId = user._id;
+              const recordIds = Array.from({ length: 10 }, (v, i) => i);
 
-          })
-      )
-
-    )
-      .finally(() => process.exit())
-
-  )
-})
+              return Promise.all(
+                categories.map(cId => {
+                  const cateId = cId._id
+                  console.log(cateId)
+                  return Promise.all(
+                    recordIds.map(rId => {
+                      console.log(rId)
+                      return Record.create({
+                        name: `Item${rId + 1}`,
+                        date: `1991-06-01`,
+                        amount: `${1000 + rId + 1}`,
+                        description: `item${rId + 1}`,
+                        userId: userId,
+                        categoryId: cateId
+                      });
+                    })
+                  )
+                })
+              );
+            })
+        )
+      );
+    })
+    .finally(() => process.exit());
+});
