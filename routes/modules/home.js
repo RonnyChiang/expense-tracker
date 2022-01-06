c71877492db2c3ce524d78d47cf6ea370e48002e
Wendell Liu <wendell.liu@houzz.com>
2022-01-06 16:11:54 +0800

    refactor: home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,98 +1,67 @@
 // express
-const express = require('express')
-const router = express.Router()
-const moment = require('moment')
+const express = require("express");
+const router = express.Router();
+const moment = require("moment");
 // introduce
-const Record = require("../../models/record")
-const Category = require('../../models/category')
-// route
-router.get('/', (req, res) => {
-  const userId = req.user._id
-  const { sortCategory, sortKeywords } = req.query
-  const keyword = sortKeywords
-  let totalAmount = 0
-  let sortCategoryId = ""
+const Record = require("../../models/record");
+const Category = require("../../models/category");
 
-  Category.find({}) //抓出資料庫所有類別
-    .then(categories => {
-      if (sortCategory) { // 有篩選存在找出id
-        sortCategoryId = categories.filter(data => data.name === sortCategory)[0]._id
+const queryRecords = (recordOption, keyword) => {
+  return Record.find(recordOption)
+    .lean()
+    .populate("categoryId")
+    .sort({ _id: "asc" })
+    .then(recordData => {
+      const keywordFind = keyword ? keyword : "";
+      console.log(keywordFind);
+      const recordsNameFilter = recordData.filter(data =>
+        data.name.toLowerCase().includes(keywordFind)
+      );
+
+      let totalAmount = 0;
+      recordsNameFilter.forEach(data => {
+        data.date = moment(data.date).format("YYYY/MM/DD");
+        totalAmount += data.amount;
+      });
+    });
+};
+
+const getRecordOptions = ({ userId, sortCategory }) => {
+  if (!sortCategory) {
+    return Promise.resolve({ userId });
+  }
+
+  return Category.findOne({ name: sortCategory }) // uncertain way to query by name, please look it up.
+    .then(category => {
+      if (!category) {
+        return { userId };
       }
-      const recordFind = sortCategoryId ? { $and: [{ userId }, { categoryId: sortCategoryId }] } : { userId } // 判斷是否有類別篩選id 
-      Record.find(recordFind)
-        .lean()
-        .populate("categoryId")
-        .sort({ _id: "asc" })
-        .then(recordData => {
-          const keywordFind = keyword ? keyword : ""
-          console.log(keywordFind)
-          const recordsNameFilter = recordData.filter(data => data.name.toLowerCase().includes(keywordFind))
-          recordsNameFilter.forEach(data => {
-            data.date = moment(data.date).format("YYYY/MM/DD")
-            totalAmount += data.amount
-          })
-          res.render("index", { recordData: recordsNameFilter, sortCategory, totalAmount, sortKeywords })
-        })
-        .catch(err => {
-          console.log(err)
-          res.render('errorPage', { status: 500, error: err.message })
-        })
+
+      return { $and: [{ userId }, { categoryId: category._id }] };
+    });
+};
+
+// route
+router.get("/", (req, res) => {
+  const userId = req.user._id;
+  const { sortCategory, sortKeywords } = req.query;
+  const keyword = sortKeywords;
+
+  getRecordOptions({ userId, sortCategory })
+    .then(recordOption => {
+      return queryRecords(recordOption, keyword);
     })
-
-
-  // if (sortCategory === "" || !sortCategory) {
-  //   Record.find({ userId })
-  //     .lean()
-  //     .populate("categoryId")
-  //     .sort({ _id: "asc" })
-  //     .then(recordData => {
-  //       if (keyword) {
-  //         const recordsNameFilter = recordData.filter(data => data.name.toLowerCase().includes(keyword))
-  //         recordsNameFilter.forEach(data => {
-  //           data.date = moment(data.date).format("YYYY/MM/DD")
-  //           totalAmount += data.amount
-  //         })
-  //         res.render("index", { recordData: recordsNameFilter, sortCategory, totalAmount, sortKeywords })
-  //       } else {
-  //         recordData.forEach(data => {
-  //           data.date = moment(data.date).format("YYYY/MM/DD")
-  //           totalAmount += data.amount
-  //         })
-  //         res.render("index", { recordData: recordData, sortCategory, totalAmount, sortKeywords })
-  //       }
-  //     })
-  //     .catch(err => {
-  //       console.log(err)
-  //       res.render('errorPage', { status: 500, error: err.message })
-  //     })
-  // } else {
-  //   return Category
-  //     .findOne({ name: sortCategory })
-  //     .then(category => category._id)
-  //     .then(sortCategoryId => {
-  //       console.log(sortCategoryId)
-  //       Record.find({ categoryId: sortCategoryId, userId })
-  //         .lean()
-  //         .populate("categoryId")
-  //         .sort({ _id: "asc" })
-  //         .then(recordData => {
-  //           const recordsNameFilter = recordData.filter(data => data.name.toLowerCase().includes(keyword))
-  //           recordsNameFilter.forEach(data => {
-  //             data.date = moment(data.date).format("YYYY/MM/DD")
-  //             totalAmount += data.amount
-  //           })
-
-  //           res.render("index", { recordData: recordsNameFilter, sortCategory, sortKeywords, totalAmount })
-  //         })
-  //         .catch(err => {
-  //           console.log(err)
-  //           res.render('errorPage', { status: 500, error: err.message })
-  //         })
-  //     })
-  // }
-
-
-
-
-})
-module.exports = router
+    .then(() => {
+      res.render("index", {
+        recordData: recordsNameFilter,
+        sortCategory,
+        totalAmount,
+        sortKeywords
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("errorPage", { status: 500, error: err.message });
+    });
+});
+module.exports = router;
