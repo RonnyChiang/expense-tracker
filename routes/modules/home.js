0f940f7ce66bf1465ef73f1e3225ac120d45a61a
ronnychiang <ronnychiang1164@gmail.com>
2022-01-06 18:10:54 +0800

    test: try to use promise

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,26 +6,38 @@ const moment = require('moment')
 const Record = require("../../models/record")
 const Category = require('../../models/category')
 // route
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   const userId = req.user._id
   const { sortCategory, sortKeywords } = req.query
   const keyword = sortKeywords
   let totalAmount = 0
-  let sortCategoryId = ""
-  const categoryItem = await Category.find().select('name').sort({ _id: 'asc' }).lean()
-  console.log(categoryItem)
-  categoryItem.forEach(item => {
-    if (item.name === sortCategory) item.selected = 'selected'
+  let sortCategoryData = undefined
+  const categoryItem = new Promise((resolve, reject) => {
+    resolve(
+      Category.find().select('name').sort({ _id: 'asc' }).lean()
+    )
   })
 
-  Category.find({}) //抓出資料庫所有類別
+  categoryItem
     .then(categories => {
       console.log(categories)
-      if (sortCategory) { // 有篩選存在找出id
-        sortCategoryId = categories.find(data => data.name === sortCategory)._id
-      }
-      console.log(sortCategoryId)
-      const recordFind = sortCategoryId ? { $and: [{ userId }, { categoryId: sortCategoryId }] } : { userId } // 判斷是否有類別篩選id 
+      categories.forEach(item => {
+        if (item.name === sortCategory) item.selected = 'selected'
+      })
+      console.log(categories)
+      console.log(categoryItem)
+      return categoryItem
+    })
+
+
+
+
+  console.log(categoryItem)
+
+  Category.find({ name: sortCategory }) //抓出資料庫所有類別
+    .then(category => {
+      sortCategoryData = category.find(data => data)
+      const recordFind = sortCategoryData ? { $and: [{ userId }, { categoryId: sortCategoryData._id }] } : { userId } // 判斷是否有類別篩選id 
       Record.find(recordFind)
         .lean()
         .populate("categoryId")
@@ -44,59 +56,6 @@ router.get('/', async (req, res) => {
           res.render('errorPage', { status: 500, error: err.message })
         })
     })
-
-
-  // if (sortCategory === "" || !sortCategory) {
-  //   Record.find({ userId })
-  //     .lean()
-  //     .populate("categoryId")
-  //     .sort({ _id: "asc" })
-  //     .then(recordData => {
-  //       if (keyword) {
-  //         const recordsNameFilter = recordData.filter(data => data.name.toLowerCase().includes(keyword))
-  //         recordsNameFilter.forEach(data => {
-  //           data.date = moment(data.date).format("YYYY/MM/DD")
-  //           totalAmount += data.amount
-  //         })
-  //         res.render("index", { recordData: recordsNameFilter, sortCategory, totalAmount, sortKeywords })
-  //       } else {
-  //         recordData.forEach(data => {
-  //           data.date = moment(data.date).format("YYYY/MM/DD")
-  //           totalAmount += data.amount
-  //         })
-  //         res.render("index", { recordData: recordData, sortCategory, totalAmount, sortKeywords })
-  //       }
-  //     })
-  //     .catch(err => {
-  //       console.log(err)
-  //       res.render('errorPage', { status: 500, error: err.message })
-  //     })
-  // } else {
-  //   return Category
-  //     .findOne({ name: sortCategory })
-  //     .then(category => category._id)
-  //     .then(sortCategoryId => {
-  //       console.log(sortCategoryId)
-  //       Record.find({ categoryId: sortCategoryId, userId })
-  //         .lean()
-  //         .populate("categoryId")
-  //         .sort({ _id: "asc" })
-  //         .then(recordData => {
-  //           const recordsNameFilter = recordData.filter(data => data.name.toLowerCase().includes(keyword))
-  //           recordsNameFilter.forEach(data => {
-  //             data.date = moment(data.date).format("YYYY/MM/DD")
-  //             totalAmount += data.amount
-  //           })
-
-  //           res.render("index", { recordData: recordsNameFilter, sortCategory, sortKeywords, totalAmount })
-  //         })
-  //         .catch(err => {
-  //           console.log(err)
-  //           res.render('errorPage', { status: 500, error: err.message })
-  //         })
-  //     })
-  // }
-
 
 
 
